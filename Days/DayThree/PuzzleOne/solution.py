732a1b5d7db0f07d6bd2320f1ad6f3b7a9a2d022
Elijah Nipper <ennipper@ncsu.edu>
2025-12-04 18:58:23 -0500

    Comments for both Day Three solutions

--- Days/DayThree/PuzzleOne/solution.py
+++ Days/DayThree/PuzzleOne/solution.py
@@ -5,22 +5,30 @@ import re
 f = open("Days/DayThree/PuzzleOne/input.txt")
 lines = f.read().split()
 
+#Hold our running sum of the max joltages
 joltageSums = 0
 
+#Go over each input
 for line in lines:
-    maxVoltage = 0
+    #Holds the current maximum found voltage
+    maxJoltage = 0
 
+    #Uses nested for loops to try every combination of two digits in our input to find the max joltage
     for i in range(len(line)):
         firstDigit = line[i]
 
         for j in range(i+1, len(line)):
             secondDigit = line[j]
 
+            #Thank you python, so easy to combine the digits and turn them into an integer
             totalDigit = int(firstDigit + secondDigit)
 
-            if totalDigit > maxVoltage:
-                maxVoltage = totalDigit
+            #Updates our maxJoltage if we found a new max
+            if totalDigit > maxJoltage:
+                maxJoltage = totalDigit
 
-    joltageSums += maxVoltage
+    #Update our running sum
+    joltageSums += maxJoltage
 
+#Prints the sum of all max joltages
 print("Max sum = " + str(joltageSums))
--- Days/DayThree/PuzzleTwo/solution.py
+++ Days/DayThree/PuzzleTwo/solution.py
@@ -5,25 +5,36 @@ import re
 f = open("Days/DayThree/PuzzleTwo/input.txt")
 lines = f.read().split()
 
+#The amount of batteries we "turn on" in each row
 joltageLen = 12
 
+#The running sum of all of our max joltages
 joltageSums = 0
 
+#Go over each input
 for line in lines:
+    #Get the length of the current input
     lineLen = len(line)
 
+    #An array which I use to keep track of the max voltage. I use this array as a makeshift stack in my algorithm
     maxJoltage = ["0"]
 
+    #Go over each digit in our current input. Algorithm keeps a running stack and pops/appends based on how many digits are left in our input
     for i in range(lineLen):
+        #Get the amount of digits left in the current input
         intsLeft = lineLen - i
+        #Tells us the index where we can start comparing ints at in maxJoltage. It is 0 at minumum and joltageLen - 1 at maximum
         startIndex = joltageLen - intsLeft
         if startIndex < 0:
             startIndex = 0
 
+        #Get the current integer we're considering from our input
         currInt = int(line[i])
 
+        #Tells us if we found a suitable digit to replace one in our stack
         appended = False
 
+        #Compares currInt to digits in maxJoltage from our startIndex to the end of maxJoltage. If we find a suitable digit, call pop k times and then append that digit
         for j in range(startIndex, len(maxJoltage)):
             currIntTwo = int(maxJoltage[j])
             
@@ -33,16 +44,21 @@ for line in lines:
                 maxJoltage.append(str(currInt))
                 appended = True
                 break
-            
+        
+        #If our currInt isn't larger than any of the digits in maxJoltage, and maxJoltage hasn't hit length yet, append our currInt to maxJoltage
         if (appended == False and len(maxJoltage) < joltageLen):
             maxJoltage.append(str(currInt))
 
     finalStringValue = ""
 
+    #Convert our maxJoltage stack to a string
     for voltage in maxJoltage:
         finalStringValue += voltage
 
+    #Convert our string to its int value
     maxJoltageInt = int(finalStringValue)
+    #Update our running sum
     joltageSums += maxJoltageInt
 
+#Print out the sum of all maximum joltages
 print("Max sum = " + str(joltageSums))
