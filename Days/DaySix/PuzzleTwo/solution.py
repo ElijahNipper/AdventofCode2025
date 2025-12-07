f88b254843e28004cdc41b91f4db1020a0237003
Elijah Nipper <elijah.n.nipper@gmail.com>
2025-12-07 11:33:35 -0500

    Commented the solution for day 6 puzzle two

--- Days/DaySix/PuzzleTwo/solution.py
+++ Days/DaySix/PuzzleTwo/solution.py
@@ -5,16 +5,21 @@ import re
 f = open("Days/DaySix/PuzzleTwo/input.txt")
 lines = f.readlines()
 
+#Gets the last line of our input, which contains all the operations (and also holds the secret to correct spacing)
 lastLine = lines[-1]
 
+#Hold the indexes to actually split each row on to preserve correct whitespace
 splitIndices = []
 
+#Gets the indices for splitIndices
 for i in range(0, len(lastLine)):
     if lastLine[i] == "*" or lastLine[i] == "+":
         splitIndices.append(i)
 
+#Holds all of our rows once they have been split on the correct spots
 rows = []
 
+#Splits all the data and puts it into rows
 for line in lines:
     currRow = []
     for i in range(0, len(splitIndices) - 1):
@@ -26,18 +31,20 @@ for line in lines:
 
     rows.append(currRow)
 
+#Uses a fancy python oneliner with zip and the unpacking operator (*) to create a new transposed array, flipping the rows and columns of the original
 transposedRows = [list(row) for row in zip(*rows)]
 
+#Replaces the blank spaces with zeros. I might get rid of this later, I don't think its actually necessary. I can do this because the input contains no 0s, but its prob not best practice
 for row in transposedRows:
     for i in range(len(row) - 1):
         row[i] = row[i].replace(" ", "0")
     row[-1] = row[-1].strip()
 
+#Holds the actual operations we will need to do once we have converted to cephalapod math
 actualOperations = []
 
-#Calculates the result of each problem and adds it to our running total
+#Converts to cephalapod math
 for line in transposedRows:
-    #print(line)
     operator = line[-1]
 
     lineLen = len(line)
@@ -59,6 +66,7 @@ for line in transposedRows:
     
     actualOperations.append(newVals)
 
+#Get rid of the zeros now that we have factored in the spacing of the significant digits
 for row in actualOperations:
     for i in range(len(row)):
         row[i] = row[i].replace("0", "")
